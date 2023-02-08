3d4cccebdc54362311c70a756a903fb6fb1cd7c0
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2023-02-08 17:45:49 +0100

    Add project files.

---
+++ Semestr1_Lekcja1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Semestr1_Lekcja1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ta instrukacja służy do wyświetlania tekstu
+            Console.WriteLine("Hello Word");
+
+            // Ta instrukcja służy do zatrzymania konsoli i pobrania danych od użytkownika
+            Console.ReadLine();
+        }
+    }
+}
